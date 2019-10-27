adefa9f4e78bdacc2917acc3c39ebc7ece60d7e0
Alejandro Lopez <alejozepol@gmail.com>
2019-10-27 11:16:47 -0500

    creacion login de html a react

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import LogoCliente from '../assets/static/logo-pizza.png';
+import Logo from '../assets/static/dexpress.png';
 
-const Login = () => <div>Login</div>;
+const Login = () => {
+  return (
+    <section className='Login'>
+      <div className='Login_logoCliente'>
+        <img src={LogoCliente} alt='Logo Cliente' />
+      </div>
+      <div className='Login__form'>
+        <div className='Login__form__logo'>
+          <img src={Logo} alt='Logo' />
+        </div>
+        <form>
+          contenido formulario
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
