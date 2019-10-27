375ee6a693c9b9d05ccadefbf2e832e846eaf1fe
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2019-10-26 23:50:02 -0500

    config de now

--- src/pages/home/home-layout.jsx
+++ src/pages/home/home-layout.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const HomeLayout = ({handleRegister,handleLogin}) => (
   <div>
     <div>Carousel</div>
+    <h1>IMAGEN1</h1>
     <div>
       <button type='button' onClick={handleRegister}>Comienza ahora</button>
       <button type='button' onClick={handleLogin}>Inicia sesión</button>
