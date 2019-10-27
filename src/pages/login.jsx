3ef7d39271825836f52fe2c31adbdf5251c8b742
Alejandro Lopez <alejozepol@gmail.com>
2019-10-27 16:46:44 -0500

    creacion pagine login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import LogoCliente from '../assets/static/logo-pizza.png';
 import Logo from '../assets/static/dexpress.png';
+import '../assets/style/pages/login.scss';
 
 const Login = () => {
   return (
     <section className='Login'>
-      <div className='Login_logoCliente'>
-        <img src={LogoCliente} alt='Logo Cliente' />
-      </div>
-      <div className='Login__form'>
-        <div className='Login__form__logo'>
-          <img src={Logo} alt='Logo' />
+      <div className='Login__container'>
+        <div className='Login__container__logoCliente'>
+          <img src={LogoCliente} alt='Logo Cliente' />
         </div>
-        <form>
-          contenido formulario
-        </form>
+        <div className='Login__container__form'>
+          <div className='Login__container__form__logo'>
+            <img src={Logo} alt='Logo' />
+          </div>
+          <div className='Login__container__form__title'>
+            <h2>Iniciar Sesión</h2>
+          </div>
+          <form>
+            <div className='FormContainer'>
+              <small className='FormLabel'>Email</small>
+              <input className='FormInput' type='email' />
+            </div>
+            <div className='FormContainer'>
+              <small className='FormLabel'>Contraseña</small>
+              <input className='FormInput' type='password' />
+            </div>
+            <button className='boton boton-primario' type='submit'>Inciar Sesión</button>
+          </form>
+        </div>
       </div>
     </section>
   );
