c8845ad456ed9e295f4e4dd7e9697719dee6f606
Wilson Romero <wilson.romero@gmail.com>
2019-10-29 08:53:53 -0500

    Dashboard - Componente header

---
+++ src/modules/dashboard/components/header/header-layout.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import {
+  UncontrolledDropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu
+} from 'reactstrap';
+
+import Logo from '../../../../assets/images/dexpress.png';
+import ProfilePic from '../../../../assets/images/profile-pic.jpg';
+
+import './header.scss';
+
+const HeaderLayout = ({
+  mobileMenuButtonClick,
+  isInFullScreen,
+  toggleFullScreen,
+  handleLogout,
+}) => (
+  <nav className='navbar fixed-top'>
+    <span>
+      <NavLink
+        to='#'
+        className='menu-button'
+        onClick={(e) => mobileMenuButtonClick(e, containerClassnames)}
+      >
+        <i className='simple-icon-menu' />
+      </NavLink>
+    </span>
+    <span className='justify-content-center'>
+      <Link to='/dashboard' className='navbar-logo'>
+        <img className='logo' src={Logo} alt='Dexpress' />
+      </Link>
+    </span>
+    <span>
+      <div className='navbar-right'>
+        <div className='header-icons d-inline-block align-middle'>
+          <button
+            className='header-icon btn btn-empty d-none d-sm-inline-block'
+            type='button'
+            id='fullScreenButton'
+            onClick={toggleFullScreen}
+          >
+            {isInFullScreen ? (
+              <i className='simple-icon-size-actual d-block' />
+            ) : (
+              <i className='simple-icon-size-fullscreen d-block' />
+            )}
+          </button>
+        </div>
+        <div className='user d-inline-block'>
+          <UncontrolledDropdown className='dropdown-menu-right'>
+            <DropdownToggle className='p-0' color='empty'>
+              <span className='name mr-1'>Sarah Connor</span>
+              <span>
+                <img alt='Profile' src={ProfilePic} />
+              </span>
+            </DropdownToggle>
+            <DropdownMenu className='mt-3' right>
+              <DropdownItem>Mi cuenta</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem onClick={() => handleLogout()}>
+                Cerrar sesión
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </div>
+      </div>
+    </span>
+  </nav>
+);
+
+export default HeaderLayout;
---
+++ src/modules/dashboard/components/header/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import HeaderLayout from './header-layout';
+
+const Header = () => <HeaderLayout />;
+
+export default Header;
+
