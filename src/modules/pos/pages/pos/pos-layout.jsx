9a632493221a92578a49b0c84ad0323c328aab07
Alejandro Lopez <alejozepol@gmail.com>
2019-10-31 20:38:46 -0500

    creacion de productos item en pos

--- src/modules/pos/pages/pos/pos-layout.jsx
+++ src/modules/pos/pages/pos/pos-layout.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../../components/header';
 import PosOrder from '../../components/pos-order';
+import PosProducts from '../../components/pos-products/pos-products';
+import PosProductsItem from '../../components/pos-products-item/pos-products-item';
+import imgProduct from '../../../../assets/imagen/pizza-min.jpg';
 import './pos-layout.scss';
 
 const posLayout = () => {
@@ -10,7 +13,25 @@ const posLayout = () => {
         <Header />
       </div>
       <div className='PosLaoyout__products'>
-      s
+        <PosProducts>
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+          <PosProductsItem img={imgProduct} name='Jamón' type='clasica' description='sss' />
+        </PosProducts>
       </div>
       <div className='PosLaoyout__order'>
         <PosOrder />
