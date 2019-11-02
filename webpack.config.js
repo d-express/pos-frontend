ea4be9530c9bf46f4002f3564f098acd37c0bce2
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2019-11-02 08:48:58 -0500

    Correccion Webpack fonts

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/',
+              outputPath: 'assets/fonts/',
             },
           }
         ],
