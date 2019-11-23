1c5fff28824f644500c7b0c64e634eb6b767ad19
Alejandro Lopez <alejozepol@gmail.com>
2019-11-22 21:06:35 -0500

    correcion error eslint

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './reducers';
-import thunk from 'redux-thunk'
 
 const configureStore = (initialState) => {
   const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
