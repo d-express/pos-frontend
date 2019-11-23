541205a948d5d3bc7e1b87477d7b39b207f5a6de
Alejandro Lopez <alejozepol@gmail.com>
2019-11-22 23:24:57 -0500

    arreglo posicion componente

---
+++ src/modules/pos/pages/select-cash-register.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SelectCashRegister from '../components/select-cash-register/index';
+
+const CashClose = () => {
+  return <SelectCashRegister />;
+};
+
+export default CashClose;
