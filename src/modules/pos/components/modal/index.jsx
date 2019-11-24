444da930fcbb9f9fcaf3ef1e312a542313698160
Alejandro Lopez <alejozepol@gmail.com>
2019-11-23 23:13:22 -0500

    style modal

--- src/modules/pos/components/modal/index.jsx
+++ src/modules/pos/components/modal/index.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Modal.scss';
 
-const Modal = ({ children, close, }) => (
-  <section className='Modal'>
-    <div className='Modal__content'>
-      <button type='button' onClick={close} className='Modal__close btn btn-outline-danger'>
-        <i className='iconsminds-close' />
-      </button>
-      {children}
-    </div>
-  </section>
-);
+const Modal = ({ children, }) => {
+  const [modalView, setModalView] = useState(false);
+
+  const view = () => (modalView ? setModalView(false) : setModalView(true));
+  return (
+    <section>
+      {modalView && (
+        <div className='Modal'>
+          <div className='Modal__content'>
+            <button type='button' onClick={() => view()} className='Modal__close btn p-0 border-0 btn-outline-danger'>
+              <i className='iconsminds-close' />
+            </button>
+            {children}
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
 
 export default Modal;
